6f529fa4246f5f2a41a64398fc3a6cac558ebd84
Guy <efrat.gay@abutech.uz>
2023-05-15 21:42:59 +0500

    feat auth_secret

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,6 +11,7 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async session({ session }) {
       await connectDb();
